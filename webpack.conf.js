aed280da9aaf0fe35a51e2c3e06249caf73f1c98
Xavier <xavier.escudero@gencat.cat>
2018-07-04 14:55:42 +0200

    Who am I first test

--- webpack.conf.js
+++ webpack.conf.js
@@ -26,8 +26,7 @@ export default {
   context: path.join(__dirname, "src"),
   entry: {
     app: ["./js/app"],
-    cms: ["./js/cms"],
-    js: ["./js/**/*"]
+    cms: ["./js/cms"]
   },
   output: {
     path: path.join(__dirname, "dist"),
